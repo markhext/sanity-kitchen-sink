ef1260e01959ce34317c6b39e6781d88983e875b
markhext <mhext@salmon.com>
2021-01-18 18:29:09 +0000

    fix: reset

--- studio/resolvePreviewUrl.js
+++ studio/resolvePreviewUrl.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development'
 
 export default function resolvePreviewUrl(document) {
-  const baseUrl = env === 'development' ? 'http://localhost:8000' : 'https://preview-sanitygatsbyblogdemo.gtsb.io'
+  const baseUrl = env === 'development' ? 'http://localhost:8000' : 'https://sanity-kitchen-sink-web-er493ms2.netlify.app'
   switch (document._type) {
     case 'route':
       if (!document.slug || !document.slug.current) {
